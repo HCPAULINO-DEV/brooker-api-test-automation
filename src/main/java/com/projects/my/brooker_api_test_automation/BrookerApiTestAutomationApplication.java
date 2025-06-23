48fa038d293eb2e2c413f81a02f2a85078b3894f
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-23 16:33:37 -0300

    FIRST COMMIT

---
+++ src/main/java/com/projects/my/brooker_api_test_automation/BrookerApiTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.projects.my.brooker_api_test_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrookerApiTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrookerApiTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/my/brooker_api_test_automation/BrookerApiTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.my.brooker_api_test_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrookerApiTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
