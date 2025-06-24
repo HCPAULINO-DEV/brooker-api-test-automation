a04576ef7fd43a249d854598009f8164a4a44e07
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-24 11:43:01 -0300

    test: adiciona classe BaseTest com setup de login

---
+++ src/test/java/com/projects/my/brooker_api_test_automation/BaseTest.java
@@ -0,0 +1,35 @@
+package com.projects.my.brooker_api_test_automation;
+
+import io.restassured.RestAssured;
+import org.apache.coyote.Response;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseTest {
+
+    protected static String token;
+
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        String json = """
+                {
+                                    "username" : "admin",
+                                    "password" : "password123"
+                                }
+                """;
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .body(json)
+                        .when()
+                        .post("/auth")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("token");
+    }
+}
