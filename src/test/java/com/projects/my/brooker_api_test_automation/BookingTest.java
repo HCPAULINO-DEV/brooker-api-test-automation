4f163eb1929690fee84695fcc81006b4f8da12b1
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-25 10:55:20 -0300

    test: valida deveAtualizarReserva na classe BookingTest

--- src/test/java/com/projects/my/brooker_api_test_automation/BookingTest.java
+++ src/test/java/com/projects/my/brooker_api_test_automation/BookingTest.java
@@ -100,7 +100,7 @@ public class BookingTest extends BaseTest{
     }
 
     @Test
-    public void deeveEfetuarReserva(){
+    public void deveEfetuarReserva(){
         String firstname = "Jim";
         String lastname = "Brown";
         Integer totalprice = 111;
@@ -137,5 +137,90 @@ public class BookingTest extends BaseTest{
                 .body("booking.totalprice", equalTo(totalprice))
                 .body("booking.depositpaid", equalTo(depositpaid))
                 .body("booking.bookingdates", notNullValue());
+    }
+
+    @Test
+    public void deveAtualizarReserva(){
+        String jsonDesatualizado = """
+                    {
+                        "firstname" : "Jim",
+                        "lastname" : "Brown",
+                        "totalprice" : 111,
+                        "depositpaid" : true,
+                        "bookingdates" : {
+                            "checkin" : "2018-01-01",
+                            "checkout" : "2019-01-01"
+                        },
+                        "additionalneeds" : "Breakfast"
+                    }
+                """;
+
+        //EFETUAR RESERVA
+        Integer idReserva =
+                given()
+                        .contentType("application/json")
+                        .body(jsonDesatualizado)
+                        .header("Cookie", "token=" + token)
+                        .log().all()
+                        .when()
+                        .post("/booking")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract()
+                        .path("bookingid");
+
+        //ATUALIZAR RESERVA
+        String firstname = "Jim-Updated";
+        String lastname = "Brown-Updated";
+        Integer totalprice = 222;
+        Boolean depositpaid = true;
+
+
+        String jsonAtualizado = """
+                    {
+                        "firstname" : "%s",
+                        "lastname" : "%s",
+                        "totalprice" : %s,
+                        "depositpaid" : %s,
+                        "bookingdates" : {
+                            "checkin" : "2018-01-01",
+                            "checkout" : "2019-01-01"
+                        },
+                        "additionalneeds" : "Breakfast"
+                    }
+                """.formatted(firstname, lastname, totalprice, depositpaid);
+
+        given()
+                .contentType("application/json")
+                .header("Cookie", "token=" + token)
+                .body(jsonAtualizado)
+                .log().all()
+                .when()
+                .put("/booking/" + idReserva)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("firstname", equalTo(firstname))
+                .body("lastname", equalTo(lastname))
+                .body("totalprice", equalTo(totalprice))
+                .body("depositpaid", equalTo(depositpaid))
+                .body("bookingdates", notNullValue());
+
+        //VERIFICAR RESERVA ATUALIZADA
+        given()
+                .accept("application/json")
+                .header("Cookie", "token=" + token)
+                .log().all()
+                .when()
+                .get("/booking/" + idReserva)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("firstname", equalTo(firstname))
+                .body("lastname", equalTo(lastname))
+                .body("totalprice", equalTo(totalprice))
+                .body("depositpaid", equalTo(depositpaid))
+                .body("bookingdates", notNullValue());
     }
 }
